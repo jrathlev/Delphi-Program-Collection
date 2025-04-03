17b5af946a3e15481e0afbc98d1733ecb3eb0a5c
Jürgen Rathlev <info@rathlev-home.de>
2025-04-03 12:49:59 +0200

    New

---
+++ components/de/Vcl.Shell.ShellConsts.pas
@@ -0,0 +1,35 @@
+﻿{*******************************************************}
+{                                                       }
+{            Delphi Visual Component Library            }
+{                                                       }
+{                  Copyright (c) 1995,                  }
+{          2001-2015 Embarcadero Technologies Inc.      }
+{                                                       }
+{*******************************************************}
+
+unit Vcl.Shell.ShellConsts;
+
+interface
+
+resourcestring
+  SShellDefaultNameStr = 'Name';
+  SShellDefaultSizeStr = 'Größe';
+  SShellDefaultTypeStr = 'Typ';
+  SShellDefaultModifiedStr = 'Geändert';
+  SShellNoDetails = 'Ordnerdetails für "%s" können nicht ermittelt werden. Fehlercode $%x';
+  SCallLoadDetails = '%s: Fehlender Aufruf von LoadColumnDetails';
+  SPalletePage = 'Samples';
+  SPropertyName = 'Stammverzeichnis';
+  SRenamedFailedError = 'Fehler beim Umbenennen in %s';
+
+const
+  SRFDesktop = 'rfDesktop'; { Do not localize }
+  SCmdVerbOpen = 'open'; { Do not localize }
+  SCmdVerbRename = 'rename'; { Do not localize }
+  SCmdVerbDelete = 'delete'; { Do not localize }
+  SCmdVerbPaste = 'paste'; { Do not localize }
+
+
+implementation
+
+end.
